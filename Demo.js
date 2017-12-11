0529fe8efc8cac5d516e79d64ee8cc19ea6a5b99
kayleersmall <kayleersmall@icloud.com>
2017-12-10 21:32:01 -0600

    Added console.log and worked with arrays

--- Demo.js
+++ Demo.js
@@ -2,4 +2,7 @@
 
 function myFunction(){
   alert("Hi!");
+  console.log("I have no clue :P");
+  var restaurants = ["Chick-Fil-A", "Azzip", "Moe's"];
+  document.getElementById("restaurants").innerHTML = restaurants;
 }
