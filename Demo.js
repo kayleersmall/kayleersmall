4c358bdc59fb47f82ebad2f2f1a8876b95a5b75d
kayleersmall <kayleersmall@icloud.com>
2017-12-15 21:40:59 -0600

    Created text box/ form that outputs result to page. created a variable that combined the inputs.

--- Demo.js
+++ Demo.js
@@ -1,8 +1,20 @@
 //alert("Hello! I am an alert box!!");
 
-function myFunction(){
+function ShowRestaurants(){
   alert("Hi!");
   console.log("I have no clue :P");
   var restaurants = ["Chick-Fil-A", "Azzip", "Moe's"];
   document.getElementById("restaurants").innerHTML = restaurants;
+}
+
+function getname(){
+  var fname = document.getElementById("fname").value;
+  var lname = document.getElementById("lname").value;
+  var combined = fname + lname;
+  console.log(fname);
+  console.log(lname);
+  console.log(combined);
+  document.getElementById("FNameResult").innerHTML = fname;
+  document.getElementById("CombinedResult").innerHTML = combined;
+
 }
