70cd2595e1ebc6fbf39ca9fd23fea08ba78490e2
cfoster5 <cfoster3204@gmail.com>
2018-01-02 21:06:36 -0600

    Added link to Marvel Characters page, added Spider-Man and Black Panther pages with images
    Still need to fix showing character when picking from select dropdown.

---
+++ marvel/marvel.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//   document.getElementById("Spidey").style.display = "none";
+// });
+//   Code above will remove the 'Spidey' div / code, but once removed you cant add
+//   it back in
+
+// function characterchange() {
+//   console.log("fired characterchange");
+//   if (document.getElementById("select").text = "spidey") {
+//     console.log("selected spidey");
+//     document.getElementById("Spidey").style.visibility = "visible";
+//   }
+// }
