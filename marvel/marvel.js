55f3fe9076875c4a7b4716e874464ac20e3edaa5
cfoster5 <cfoster3204@gmail.com>
2018-01-02 23:39:48 -0600

    Fixed problem with select dropdown
    Fix was changing
    if (document.getElementById("select").text = "spidey") {}
    to
    creating a variable called character and showing it's div if the character equaled a certain selection.

--- marvel/marvel.js
+++ marvel/marvel.js
@@ -4,10 +4,22 @@
 //   Code above will remove the 'Spidey' div / code, but once removed you cant add
 //   it back in
 
-// function characterchange() {
-//   console.log("fired characterchange");
-//   if (document.getElementById("select").text = "spidey") {
-//     console.log("selected spidey");
-//     document.getElementById("Spidey").style.visibility = "visible";
-//   }
-// }
+function characterchange() {
+  console.log("fired characterchange");
+
+  var character = document.getElementById("select").value;
+  console.log(character);
+
+  if (character == "spidey") {
+    console.log("selected spidey");
+    document.getElementById("Spidey").style.display = "inline";
+    document.getElementById("Panther").style.display = "none";
+    console.log("showing Spidey");
+  }
+  if (character == "panther") {
+    console.log("selected panther");
+    document.getElementById("Spidey").style.display = "none";
+    document.getElementById("Panther").style.display = "inline";
+    console.log("showing Panther");
+  }
+}
