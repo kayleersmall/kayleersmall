f017b3e1cdcf59c3f5ea39f77db72ac387f5677d
cfoster5 <cfoster3204@gmail.com>
2018-01-27 20:32:08 -0600

    Added Moment.js to get date and added movie JSON object

---
+++ index.js
@@ -0,0 +1,18 @@
+//JSON - like arrays on steroids
+var movies = {
+  "Black Panther" : {
+    "title" : "Black Panther",
+    "theatricalrelease" : "2/16/2018"
+  },
+  "Infinity War" : {
+    "title" : "Avengers: Infinity War",
+    "theatricalrelease" : "5/4/2018"
+  },
+  "Captain Marvel" : {
+    "title" : "Captain Marvel",
+    "theatricalrelease" : "TBA"
+  }
+}
+
+console.log(movies);
+console.log(corey);
