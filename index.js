24b6291a9e6b113231268ab65b3e23fe38268933
cfoster5 <cfoster3204@gmail.com>
2018-02-03 20:56:53 -0600

    Added fake search list that hides on load and shows when search input != null. Intro to loops. Correctly looping through JSON to get movie title and movie theatricalreleasedate.

--- index.js
+++ index.js
@@ -14,5 +14,35 @@ var movies = {
   }
 }
 
+var characters = ["Spider-Boy", "Iron Man", "Hawkeye", "Dr. Strange", "Thor", "Hulk", "Black Panther", "Vision"];
+
+console.log(characters)
+
+console.log(characters[0]); //Logs Spider-Boy
+console.log(characters[1]); //Logs Iron Man
+console.log(characters[2]);
+console.log(characters[3]);
+
+for (var i = 0; i < characters.length; i++) { //Logs entire array from beginning to end
+  console.log(characters[i]);
+}
+
+
 console.log(movies);
-console.log(corey);
+console.log(moment().format('l'));
+console.log(Object.keys(movies).length);
+
+for(var key in movies) {
+  console.log(movies[key]);
+  console.log(movies[key].title); //logs correctly
+  console.log(movies[key].theatricalrelease);
+}
+
+function showsearchlist(){
+  // alert("key was pressed")
+  document.getElementById('searchlist').style.display = 'inline';
+  var searchinput = document.getElementById("searchbar").value;
+  if (searchinput == null || searchinput == "") {
+    document.getElementById('searchlist').style.display = 'none';
+  }
+}
